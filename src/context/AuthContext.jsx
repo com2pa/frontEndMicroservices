f78f99209b641b10e1656b0cc771fb9ad489de9b
merwil vegas <com2pa@gmail.com>
2025-02-17 10:13:31 -0400

    se modifico

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,8 +1,8 @@
 import { createContext, useState } from "react";
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext();
 
-export const AuthProvider = ({ children }) => {
+const AuthProvider = ({children}) => {
   const [auth, setAuth] = useState({});
   return (
     <AuthContext.Provider value={{ auth, setAuth }}>
